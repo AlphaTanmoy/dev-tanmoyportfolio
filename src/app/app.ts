967d5b6d380920dffab78600247978acce225a4b
AlphaTanmoy <dtanmoy169@gmail.com>
2025-12-23 00:12:47 +0530

    changes

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,31 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { MobileNavbarComponent } from './components/mobile-navbar/mobile-navbar.component';
+import { MobileTopNavbarComponent } from './components/mobile-top-navbar/mobile-top-navbar.component';
+import { HeroComponent } from './components/hero/hero.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { AboutComponent } from './components/about/about.component';
+import { ExperienceComponent } from './components/experience/experience.component';
+import { EducationComponent } from './components/education/education.component';
+import { AchievementsComponent } from './components/achievements/achievements.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    NavbarComponent,
+    MobileNavbarComponent,
+    MobileTopNavbarComponent,
+    HeroComponent,
+    SkillsComponent,
+    AboutComponent,
+    ExperienceComponent,
+    EducationComponent,
+    AchievementsComponent,
+    ProjectsComponent,
+    ContactComponent
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
