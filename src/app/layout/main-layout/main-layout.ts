e7585f127ab1f235506123fca616c6f272714284
AlphaTanmoy <dtanmoy169@gmail.com>
2026-07-21 03:16:23 +0530

    other sections added

--- src/app/layout/main-layout/main-layout.ts
+++ src/app/layout/main-layout/main-layout.ts
@@ -2,6 +2,12 @@ import { Component } from '@angular/core';
 import { Navbar } from '../navbar/navbar';
 import { Footer } from '../footer/footer';
 import { Hero } from '../../features/hero/hero';
+import { About } from '../../features/about/about';
+import { Contact } from '../../features/contact/contact';
+import { Education } from '../../features/education/education';
+import { Skills } from '../../features/skills/skills';
+import { Projects } from '../../features/projects/projects';
+import { Experience } from '../../features/experience/experience';
 
 @Component({
   selector: 'app-main-layout',
@@ -9,7 +15,13 @@ import { Hero } from '../../features/hero/hero';
   imports: [
     Navbar,
     Hero,
-    Footer
+    Footer,
+    About,
+    Experience,
+    Projects,
+    Skills,
+    Education,
+    Contact
   ],
   templateUrl: './main-layout.html',
   styleUrl: './main-layout.scss'
