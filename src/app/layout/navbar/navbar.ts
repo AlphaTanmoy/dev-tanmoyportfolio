92d083d44cdd71a500d56506506a28a4cab8d3c4
AlphaTanmoy <dtanmoy169@gmail.com>
2026-07-21 03:30:36 +0530

    changes for footer and navbar

--- src/app/layout/navbar/navbar.ts
+++ src/app/layout/navbar/navbar.ts
@@ -1,11 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, signal } from '@angular/core';
+import { TitleCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  standalone: true,
+  imports: [TitleCasePipe],
   templateUrl: './navbar.html',
-  styleUrl: './navbar.scss',
+  styleUrl: './navbar.scss'
 })
 export class Navbar {
 
+  activeSection = signal('about');
+
+  sections = [
+    'about',
+    'experience',
+    'projects',
+    'skills',
+    'education',
+    'contact'
+  ];
+
+  scrollTo(id: string) {
+    document.getElementById(id)?.scrollIntoView({
+      behavior: 'smooth'
+    });
+  }
+
+  @HostListener('window:scroll')
+  onScroll() {
+
+    for (const section of this.sections) {
+
+      const element = document.getElementById(section);
+
+      if (!element) continue;
+
+      const rect = element.getBoundingClientRect();
+
+      if (rect.top <= 120 && rect.bottom >= 120) {
+        this.activeSection.set(section);
+      }
+    }
+  }
 }
