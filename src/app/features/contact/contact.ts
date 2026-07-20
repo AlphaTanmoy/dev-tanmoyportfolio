98e62da0ff52eafa709e3298e6e9daa877194a08
AlphaTanmoy <dtanmoy169@gmail.com>
2026-07-21 04:25:08 +0530

    contact us done

--- src/app/features/contact/contact.ts
+++ src/app/features/contact/contact.ts
@@ -2,10 +2,35 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  standalone: true,
   templateUrl: './contact.html',
-  styleUrl: './contact.scss',
+  styleUrl: './contact.scss'
 })
 export class Contact {
+
+  readonly email = 'tanmoy.official.24@gmail.com';
+
+  copyEmail(): void {
+
+    navigator.clipboard.writeText(this.email);
+
+    alert('Email copied successfully.');
+
+  }
+
+  downloadResume(): void {
+
+    window.open('/assets/resume/Tanmoy_Das_Resume.pdf', '_blank');
+
+  }
+
+  sendMail(): void {
+
+    window.open(
+      'mailto:tanmoy.official.24@gmail.com',
+      '_blank'
+    );
+
+  }
 
 }
